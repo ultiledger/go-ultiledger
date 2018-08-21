6f2b0beca0d0ee9c00ee46b7c3fdee565f2062df
bobonovski <zhaocc.act@gmail.com>
2018-08-21 16:09:49 +0800

    save nodeID from healthcheck request

--- api/ultnode_server.go
+++ api/ultnode_server.go
@@ -7,14 +7,19 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 
+	"github.com/ultiledger/go-ultiledger/crypto"
 	pb "github.com/ultiledger/go-ultiledger/ultpb"
 	"github.com/ultiledger/go-ultiledger/ultpb/rpc"
 )
 
 type ULTNodeServer struct {
-	IP           string             // ip address of this node
-	NodeID       string             // ID of this node (public key)
-	peerChan     chan string        // channel for adding new peer IP
+	IP     string // ip address of this node
+	NodeID string // ID of this node (public key)
+
+	nodeKey map[string]*crypto.ULTKey
+
+	peerChan chan string // channel for adding new peer IP
+
 	txChan       chan *pb.Tx        // channel for transaction submission
 	nominateChan chan *pb.Statement // channel for nomination statement
 }
@@ -30,8 +35,13 @@ func (s *ULTNodeServer) HealthCheck(ctx context.Context, req *rpc.HealthCheckReq
 	if !ok {
 		return resp, errors.New("failed to retrieve incoming context")
 	}
-	if len(md.Get("IP")) > 0 {
+	if len(md.Get("IP")) > 0 && len(md.Get("NodeID")) > 0 {
 		s.peerChan <- md.Get("IP")[0]
+		k, err := crypto.DecodeKey(md.Get("NodeID")[0])
+		if err != nil {
+			return resp, errors.New("invalid node ID")
+		}
+		s.nodeKey[md.Get("IP")[0]] = k
 	}
 	grpc.SendHeader(ctx, metadata.Pairs("IP", s.IP, "NodeID", s.NodeID))
 	return resp, nil
