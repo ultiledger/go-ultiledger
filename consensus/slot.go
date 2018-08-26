b5824fc4d35e4eaf9653a8b72ab743594ee6e7d3
bobonovski <zhaocc.act@gmail.com>
2018-08-26 22:02:53 +0800

    add nomination assembling before broadcast

--- consensus/slot.go
+++ consensus/slot.go
@@ -26,6 +26,9 @@ type Slot struct {
 	// nomination round
 	round int
 
+	// latest nomination
+	latestNomination *pb.Nomination
+
 	votes       mapset.Set
 	accepts     mapset.Set
 	candidates  mapset.Set
@@ -46,11 +49,57 @@ func newSlot(idx uint64, nodeID string, l *zap.SugaredLogger, pm *peer.Manager) 
 	return s
 }
 
+// nominate a consensus value for this slot
 func (s *Slot) nominate(quorum *pb.Quorum, prevHash, currHash string) (string, error) {
 	s.round++
 	// TODO(bobonovski) compute leader weights
-	// leaders := s.pm.GetLiveNodeID()
+	s.votes.Add(currHash) // For test
 	return "", nil
+}
+
+// assemble a nomination and broadcast it to other peers
+func (s *Slot) sendNomination(quorum *pb.Quorum, quorumHash string) error {
+	// create an abstract nomination statement
+	nom := &pb.Nomination{
+		QuorumHash: quorumHash,
+	}
+	for vote := range s.votes.Iter() {
+		nom.VoteList = append(nom.VoteList, vote.(string))
+	}
+	for accept := range s.accepts.Iter() {
+		nom.AcceptList = append(nom.AcceptList, accept.(string))
+	}
+	/*
+		nomBytes, err := pb.Encode(nom)
+		if err != nil {
+			return err
+		}
+		stmt := &pb.Statement{
+			StatementType: pb.Statement_NOMINATE,
+			NodeID:        s.nodeID,
+			SlotIndex:     s.index,
+			Data:          nomBytes,
+		}
+	*/
+	// promote local votes
+	s.addNomination(s.nodeID, nom) // skip error checking
+	acceptUpdated, candidateUpdated, err := s.promoteVotes(quorum, nom)
+	if err != nil {
+		return err
+	}
+	// broadcast the nomination if it is a new one
+	if isNewerNomination(s.latestNomination, nom) {
+		// TODO(bobonovksi) broadcast nomination
+	}
+	// send new nomination if votes changed
+	if acceptUpdated {
+		s.sendNomination(quorum, quorumHash)
+	}
+	// start balloting if candidates changed
+	if candidateUpdated {
+		// TODO(bobonovski) balloting
+	}
+	return nil
 }
 
 // check whether the input nomination is valid and newer
@@ -62,16 +111,22 @@ func (s *Slot) addNomination(nodeID string, newNom *pb.Nomination) error {
 	// check whether the existing nomination of the remote node
 	// is the proper subset of the new nomination
 	if nom, ok := s.nominations[nodeID]; ok {
-		if !IsProperSubset(nom.VoteList, newNom.VoteList) {
-			// TODO(bobonovski) more elaborate check like interset?
-			return fmt.Errorf("old votes is not proper set of new votes")
-		}
-		if !IsProperSubset(nom.AcceptList, newNom.AcceptList) {
-			return fmt.Errorf("old accepts is not proper set of new accepts")
+		if isNewerNomination(nom, newNom) {
+			s.nominations[nodeID] = newNom
 		}
 	}
-	s.nominations[nodeID] = newNom
 	return nil
+}
+
+func isNewerNomination(anom *pb.Nomination, bnom *pb.Nomination) bool {
+	if !IsProperSubset(anom.VoteList, bnom.VoteList) {
+		// TODO(bobonovski) more elaborate check like interset?
+		return false
+	}
+	if !IsProperSubset(anom.AcceptList, bnom.AcceptList) {
+		return false
+	}
+	return true
 }
 
 func isVblocking(quorum *pb.Quorum, nodeSet mapset.Set) bool {
@@ -160,6 +215,9 @@ func findVoteOrAcceptNodes(v string, noms map[string]*pb.Nomination) mapset.Set 
 func (s *Slot) promoteVotes(quorum *pb.Quorum, newNom *pb.Nomination) (bool, bool, error) {
 	acceptUpdated := false
 	for _, vote := range newNom.VoteList {
+		if s.accepts.Contains(vote) {
+			continue
+		}
 		ns := findAcceptNodes(vote, s.nominations)
 		// use federated vote to promote value
 		if !isVblocking(quorum, ns) {
