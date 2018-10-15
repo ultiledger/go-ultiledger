09dc9d110ca5ddf596e3bc7947cad3e7ad92d928
ult-bobonovski <alex@ultiledger.io>
2018-10-15 22:59:23 +0800

    add http response filter

--- cmd/ulthub/service/handler.go
+++ cmd/ulthub/service/handler.go
@@ -2,8 +2,11 @@ package service
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/emicklei/go-restful"
+
+	"github.com/ultiledger/go-ultiledger/log"
 )
 
 // NewHandler creates a customized http handler to the http server.
@@ -15,10 +18,21 @@ func NewHandler(coreEndpoints string) http.Handler {
 		Consumes(restful.MIME_JSON).
 		Produces(restful.MIME_JSON)
 	ws.Route(ws.POST("/tx").To(hub.SummitTx))
-	ws.Route(ws.GET("/tx").To(hub.QueryTx))
+	ws.Route(ws.GET("/tx").To(hub.QueryTx)).Filter(responseFilter)
 
 	container := restful.NewContainer()
 	container.Add(ws)
 
 	return container
+}
+
+func responseFilter(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
+	start := time.Now()
+	chain.ProcessFilter(req, resp)
+	end := time.Now().Sub(start)
+
+	if resp.StatusCode() != 200 {
+		log.Errorw("failed", "statusCode", resp.StatusCode(), "method", req.Request.Method, "URL", req.Request.URL, "errorMsg", resp.Error(), "responseTime", end)
+	}
+	log.Infow("success", "statusCode", resp.StatusCode(), "method", req.Request.Method, "URL", req.Request.URL, "responseTime", end)
 }
