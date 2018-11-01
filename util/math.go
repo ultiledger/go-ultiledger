0a18e6d04775ad66657a9dda7edaf583072a51e1
ult-bobonovski <alex@ultiledger.io>
2018-11-01 13:33:24 +0800

    move argsorter to util package

--- util/math.go
+++ util/math.go
@@ -15,3 +15,19 @@ func MinUint64(x uint64, y uint64) uint64 {
 	}
 	return y
 }
+
+// Find the max between two int64 values
+func MaxInt64(x int64, y int64) int64 {
+	if x >= y {
+		return x
+	}
+	return y
+}
+
+// Find the min between two int64 values
+func MinInt64(x int64, y int64) int64 {
+	if x <= y {
+		return x
+	}
+	return y
+}
---
+++ util/sorter_test.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStringArgSort(t *testing.T) {
+	names := []string{"Bob", "Alex", "Zara", "July"}
+	s := NewStringSlice(true, names...)
+	sort.Sort(s)
+
+	assert.Equal(t, s.Idx, []int{2, 3, 0, 1})
+}
+
+func TestIntArgSort(t *testing.T) {
+	ints := []int{2, 4, 6, 3, 2, 1}
+	is := NewIntSlice(false, ints...)
+	sort.Sort(is)
+
+	assert.Equal(t, is.Idx, []int{5, 0, 4, 3, 1, 2})
+}
+
+func TestFloat64ArgSort(t *testing.T) {
+	floats := []float64{2.0, 4.0, 7.0, 1.0, 2.5}
+	fs := NewFloat64Slice(true, floats...)
+	sort.Sort(fs)
+
+	assert.Equal(t, fs.Idx, []int{2, 1, 4, 0, 3})
+}
