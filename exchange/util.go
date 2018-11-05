7fc7e5cb5963b090fdc51b286ac94858a30a388c
ult-Scott <jiqiu.xu@ultiledger.io>
2018-11-05 18:20:44 +0800

    add big int arithmetics util

--- exchange/util.go
+++ exchange/util.go
@@ -6,10 +6,46 @@ import (
 	"github.com/ultiledger/go-ultiledger/ultpb"
 )
 
+type RoundType uint8
+
+const (
+	RoundUp RoundType = iota
+	RoundDown
+)
+
 // Compare two price
 func ComparePrice(lhs *ultpb.Price, rhs *ultpb.Price) int {
 	l := big.NewRat(lhs.Numerator, lhs.Denominator)
 	r := big.NewRat(rhs.Numerator, rhs.Denominator)
 
 	return l.Cmp(r)
+}
+
+// Multiply two int64 number
+func MultiplyInt64(lhs int64, rhs int64) *big.Int {
+	l := big.NewInt(lhs)
+	r := big.NewInt(rhs)
+
+	result := big.NewInt(0)
+	result.Mul(l, r)
+
+	return result
+}
+
+// Divide the value by an int64 number and round
+// the result based on RoundType
+func DivideBigInt(val *big.Int, divisor int64, roundType RoundType) int64 {
+	div := big.NewInt(divisor)
+
+	quotient := big.NewInt(0)
+	modulus := big.NewInt(0)
+	quotient.DivMod(val, div, modulus)
+
+	q := quotient.Int64()
+	remainder := modulus.Int64()
+
+	if roundType == RoundUp && remainder > 0 {
+		return q + 1
+	}
+	return q
 }
