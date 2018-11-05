2c67f969655ad41f63b3f7f810e26a28a02ec458
ult-Scott <jiqiu.xu@ultiledger.io>
2018-11-05 18:21:06 +0800

    change asset amount data type to int64

--- exchange/order.go
+++ exchange/order.go
@@ -9,15 +9,15 @@ type Order struct {
 	// asset to sell
 	SellAsset *ultpb.Asset
 	// max amount of SellAsset we can sell
-	MaxSellAmount uint64
+	MaxSellAmount int64
 	// asset to buy
 	BuyAsset *ultpb.Asset
 	// max amount of BuyAmount we can buy
-	MaxBuyAmount uint64
+	MaxBuyAmount int64
 	// price of SellAsset in terms of BuyAsset
 	Price *ultpb.Price
 	// defacto amount of SellAsset we have sold after filling order
-	SoldAmount uint64
+	SoldAmount int64
 	// defacto amount of BuyAsset we have bought after filling order
-	BoughtAmount uint64
+	BoughtAmount int64
 }
