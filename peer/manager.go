289a4d9566427d63fc6ba4ed84b56055a286ad6b
ult-bobonovski <alex@ultiledger.io>
2019-08-08 18:58:40 +0800

    Add WithBackoffMaxDelay DialOption to the gRPC dialer.

--- peer/manager.go
+++ peer/manager.go
@@ -143,7 +143,7 @@ func (pm *Manager) AddPeerAddr(addr string) {
 
 // Connects the remote peer with provided network address.
 func (pm *Manager) connectPeer(addr string) (*Peer, error) {
-	conn, err := grpc.Dial(addr, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(time.Second))
+	conn, err := grpc.Dial(addr, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(time.Second), grpc.WithBackoffMaxDelay(200*time.Millisecond))
 	if err != nil {
 		return nil, err
 	}
